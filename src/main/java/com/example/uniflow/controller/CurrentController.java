4f6172f3df988a435a4e34652270d96f29e905d8
laith <laithswais@gmail.com>
2024-06-17 19:21:02 +0300

    current api, app properties

--- src/main/java/com/example/uniflow/controller/CurrentController.java
+++ src/main/java/com/example/uniflow/controller/CurrentController.java
@@ -1,7 +1,5 @@
 package com.example.uniflow.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +16,13 @@ public class CurrentController {
 
     @GetMapping("/current")
     public Map<String, String> getCurrent() {
-        String url = "https://run.mocky.io/v3/3349db11-4159-4f58-b928-3f72aa5c0337";
-        Map<String, String> response = restTemplate.getForObject(url, Map.class);
+        String url = "http://localhost:8000/count";
+        Map<String, Object> response = restTemplate.getForObject(url, Map.class);
 
         // Construct the response in the required format
         Map<String, String> result = new HashMap<>();
-        result.put("current_gas", response.get("current_gas"));
-        result.put("current_diesel", response.get("current_diesel"));
+        result.put("current_gas", String.valueOf(response.get("current_gas")));
+        result.put("current_diesel", String.valueOf(response.get("current_diesel")));
 
         return result;
     }
